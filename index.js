0f1c6273da5b530202d9e1add056a06a184a2fb8
bambi teague <teeegsa@gmail.com>
2021-08-14 14:45:26 -0500

    starting up frontend

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hi JS!");
+document.addEventListener("DOMContentLoaded", (event) => {
+  fetch("http://localhost:3000/users")
+    .then((response) => response.json())
+    .then((data) => displayUsers(data));
+  // const userButton = document.getElementById("butt-on");
+  // userButton.addEventListener("click", getUsers);
+
+  function displayUsers(users) {
+    console.log(users);
+  }
+});
+
+
+//GET YOUR DELIVERABLES DONE FIRST!
