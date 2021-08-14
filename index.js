d894a748b4233cc049c0ccd48d62de6a8e826172
bambi teague <teeegsa@gmail.com>
2021-08-14 18:24:45 -0500

    edit image display size

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
   fetch("http://localhost:3000/users")
     .then((response) => response.json())
     .then((data) => displayUsers(data));
-  // const userButton = document.getElementById("butt-on");
-  // userButton.addEventListener("click", getUsers);
+  const userButton = document.getElementById("butt-on");
+  userButton.addEventListener("click", () => {
+    fetch("https://source.unsplash.com/random").then((response) => {
+      const randomImg = document.getElementById("random-img");
+      randomImg.setAttribute("src", response.url)
+    });
+  });
 
   function displayUsers(users) {
     console.log(users);
   }
 });
-
 
 //GET YOUR DELIVERABLES DONE FIRST!
