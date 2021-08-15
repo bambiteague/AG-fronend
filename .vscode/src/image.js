7e1c0f5bab78d5f87b57109e1f1160dc7f611e0b
bambi teague <teeegsa@gmail.com>
2021-08-15 13:01:50 -0500

    setting attributes for images

--- .vscode/src/image.js
+++ .vscode/src/image.js
@@ -3,5 +3,9 @@
 // setting the properties of each image and the html we want to put on the DOM for each item
 // attach event listeners particular to each Image
 class Image {
-
+  constructor({url, description}){
+    //setting the properties of each image
+    this.url = url
+    this.description = description
+  }
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // will trigger the code from our classes, etc. 
 
 document.addEventListener("DOMContentLoaded", (event) => {
+  
   fetch("http://localhost:3000/images")   
     .then((response) => response.json())
     .then((data) => displayImages(data));
