2523124c50e1b9084e394eea7315064bae680488
bambi teague <teeegsa@gmail.com>
2021-08-15 12:26:29 -0500

    creating blank .js files for front end

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  fetch("http://localhost:3000/users")
-    .then((response) => response.json())
-    .then((data) => displayUsers(data));
+  // fetch("http://localhost:3000/users")   
+  //   .then((response) => response.json())
+  //   .then((data) => displayUsers(data));
 
-  // const userButton = document.getElementById("butt-on");
-  // userButton.addEventListener("click", () => {
-  //   fetch("https://source.unsplash.com/random").then((response) => {
-  //     const randomImg = document.getElementById("random-img");
-  //     randomImg.setAttribute("src", response.url)
-  //   });
-  // });
-
-  function displayUsers(users) {
-    console.log(users);
-  }
+  // function displayUsers(users) {
+  //   console.log(users);
+  // }
 });
-
-//GET YOUR DELIVERABLES DONE FIRST!
