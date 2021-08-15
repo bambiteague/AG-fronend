68742069de8174afdcf6d41e147c2bf22d15c186
bambi teague <teeegsa@gmail.com>
2021-08-15 11:44:03 -0500

    commenting out silly testing code

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
   fetch("http://localhost:3000/users")
     .then((response) => response.json())
     .then((data) => displayUsers(data));
-  const userButton = document.getElementById("butt-on");
-  userButton.addEventListener("click", () => {
-    fetch("https://source.unsplash.com/random").then((response) => {
-      const randomImg = document.getElementById("random-img");
-      randomImg.setAttribute("src", response.url)
-    });
-  });
+
+  // const userButton = document.getElementById("butt-on");
+  // userButton.addEventListener("click", () => {
+  //   fetch("https://source.unsplash.com/random").then((response) => {
+  //     const randomImg = document.getElementById("random-img");
+  //     randomImg.setAttribute("src", response.url)
+  //   });
+  // });
 
   function displayUsers(users) {
     console.log(users);
