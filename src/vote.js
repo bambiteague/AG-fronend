64daf211c030f2573161079739a38a738d9f39c8
bambi teague <teeegsa@gmail.com>
2021-08-23 08:39:20 -0500

    commenting out vote.js, but hanging on to file for now

--- src/vote.js
+++ src/vote.js
@@ -1,18 +1,18 @@
-const post = Post;
-const POST_URL = "http://localhost:3000/posts";
-const EMPTY_VOTE = "▼";
-const FULL_VOTE = "▲";
+// const post = Post;
+// const POST_URL = "http://localhost:3000/posts";
+// const EMPTY_VOTE = "▼";
+// const FULL_VOTE = "▲";
 
-class Vote {
-  static postBtn = document.getElementById("post-button");
+// class Vote {
+//   static postBtn = document.getElementById("post-button");
 
-  constructor(upVote, downVote) {
-    this.upVote = upVote;
-    this.downVote = downVote;
-  }
+//   constructor(upVote, downVote) {
+//     this.upVote = upVote;
+//     this.downVote = downVote;
+//   }
 
-  // need to add to each postCard Like I did with the delete button for Posts
-  // need event listeners
+//   // need to add to each postCard Like I did with the delete button for Posts
+//   // need event listeners
 
 
 }
