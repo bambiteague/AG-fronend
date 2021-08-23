43e597c77a0a982b8126ca2b2922ee493cff64df
bambi teague <teeegsa@gmail.com>
2021-08-23 06:40:29 -0500

    starting Vote class / notes to direct process

--- src/vote.js
+++ src/vote.js
@@ -1,1 +1,11 @@
+class Vote {
+  constructor(upVote, downVote) {
+    this.upVote = upVote;
+    this.downVote = downVote;
+  }
+  // create a up arrow/down arrow symbol/button
+  // create onclick event listener
+  // on click, up or down vote counter should change accordingly and show count next to the symbol
 
+  // IF upVote has been selected & THEN downVote is clicked, deselect that upVote (yeah? this one is less important/not necessary for deliverables)
+}
