dbb11312ad5e250fd22902bb13a9fe9bbb0f3613
bambi teague <teeegsa@gmail.com>
2021-08-23 07:06:24 -0500

    more added to Vote class

--- src/vote.js
+++ src/vote.js
@@ -1,22 +1,28 @@
-const id = Post.id;
+const post = Post;
 class Vote {
-  static post(id) {
-    const post = Post.findbyid[id];
+  static postById(id) {
+    post.id;
   }
 
-  static upVote() {}
+  static upVote() {
+    post.votes.upVote;
+  }
 
-  static downVote() {}
+  static downVote() {
+    post.votes.downVote;
+  }
 
   constructor(upVote, downVote) {
     this.upVote = upVote;
     this.downVote = downVote;
   }
 
+  addUpVote() {}
+
+  addDownVote() {}
   // a Post has_many Votes
   // create a up arrow/down arrow symbol/button
   // create onclick event listener
   // on click, up or down vote counter should change accordingly and show count next to the symbol
-
   // IF upVote has been selected & THEN downVote is clicked, deselect that upVote (yeah? this one is less important/not necessary for deliverables)
 }
