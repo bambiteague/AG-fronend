073c8f87c066e0a1f14f2834f78dc9790c30bede
bambi teague <teeegsa@gmail.com>
2021-08-23 07:37:36 -0500

    create html element for up and down votes / adding arrow glyphs

--- src/vote.js
+++ src/vote.js
@@ -1,32 +1,16 @@
 const post = Post;
+const POST_URL = "http://localhost:3000/posts";
+const EMPTY_VOTE = '▼'
+const FULL_VOTE = '▲'
+
 class Vote {
-  static postById(id) {
-    const postCard = post.id ;
-  }
-
-  static upVote() {
-    post.votes.upVote;
-  }
-
-  static downVote() {
-    post.votes.downVote;
-  }
-
+  
   constructor(upVote, downVote) {
     this.upVote = upVote;
     this.downVote = downVote;
   }
 
-  addUpVote() {
-    document.addEventListener("on click", )
-  }
-
-  addDownVote() {
-    document.addEventListener("on click", )
-  }
-  // a Post has_many Votes
-  // create a up arrow/down arrow symbol/button
-  // create onclick event listener
-  // on click, up or down vote counter should change accordingly and show count next to the symbol
-  // IF upVote has been selected & THEN downVote is clicked, deselect that upVote (yeah? this one is less important/not necessary for deliverables)
 }
+
+// glyphicon glyphicon-arrow-down	
+// glyphicon glyphicon-arrow-up
