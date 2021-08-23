1d22f30b569caaa708be3922546498dd8df2fea3
bambi teague <teeegsa@gmail.com>
2021-08-23 07:00:15 -0500

    constructor/begginning static methods for class

--- src/vote.js
+++ src/vote.js
@@ -1,8 +1,19 @@
+const id = Post.id;
 class Vote {
+  static post(id) {
+    const post = Post.findbyid[id];
+  }
+
+  static upVote() {}
+
+  static downVote() {}
+
   constructor(upVote, downVote) {
     this.upVote = upVote;
     this.downVote = downVote;
   }
+
+  // a Post has_many Votes
   // create a up arrow/down arrow symbol/button
   // create onclick event listener
   // on click, up or down vote counter should change accordingly and show count next to the symbol
